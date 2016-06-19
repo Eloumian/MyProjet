0663d33b5cd898875040f43c23063fd6953281b4
Eloumian <maroaslan@hotmail.com>
2016-06-19 14:44:43 +0200

    my second commit

--- testGit/src/testGit/testGit.java
+++ testGit/src/testGit/testGit.java
@@ -5,7 +5,7 @@ public class testGit {
 	public static void main(String[] args) {
 		int[] array = {1,2,3};	
 		try{
-			System.out.println("Test git  : " + array[0]);
+			System.out.println("Test git  gg: " + array[0]);
 		}catch(Exception e) {
 			System.out.println("pas ok");
 		}

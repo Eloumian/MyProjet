8306530a182697f2809833d928ebce0174e9bbf1
Eloumian <maroaslan@hotmail.com>
2016-06-19 14:40:30 +0200

    My first project

---
+++ testGit/src/testGit/testGit.java
@@ -0,0 +1,15 @@
+package testGit;
+
+public class testGit {
+
+	public static void main(String[] args) {
+		int[] array = {1,2,3};	
+		try{
+			System.out.println("Test git  : " + array[0]);
+		}catch(Exception e) {
+			System.out.println("pas ok");
+		}
+		
+	}
+
+}
